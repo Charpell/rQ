85525bb5eb80bf91f4e4e7ed5ecd0f46556d2a01
kxngnathan <jonathanojohnson@gmail.com>
2020-03-09 18:16:53 +0100

    Side Bar changes

--- src/navigation/HomeDrawer.js
+++ src/navigation/HomeDrawer.js
@@ -3,6 +3,7 @@ import { createDrawerNavigator } from "react-navigation-drawer";
 
 import BottomTabs from "./src";
 import SideBar from "../components/SideBar";
+import { SIZES } from "../utils/theme";
 import { CloseButton } from "../components";
 import TransactionSummary from "../screens/Transactions/TransactionSummary";
 
@@ -12,7 +13,7 @@ export default createDrawerNavigator(
   },
   {
     contentComponent: props => <SideBar {...props} />,
-    drawerWidth: 269,
+    drawerWidth: SIZES.width * 0.7,
     drawerBackgroundColor: "transparent",
     contentOptions: {
       activeBackgrundColor: "transparent"
